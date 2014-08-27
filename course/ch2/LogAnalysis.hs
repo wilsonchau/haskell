716c26e04cfec81fdea8b23fa078486f5b2309b0
Wilson Chau <wchau@kabam.com>
2014-08-27 12:54:38 -0700

    Ch 2

--- course/ch2/LogAnalysis.hs
+++ course/ch2/LogAnalysis.hs
@@ -6,22 +6,23 @@ parseMessageType :: Char -> String -> MessageType
 parseMessageType 'I' _ = Info
 parseMessageType 'W' _ = Warning
 parseMessageType 'E' x = Error ((read (head (words x))) :: Int)
-parseMessageType x y = Info
 
 parseTimestamp :: String => TimeStamp
-parseTimestamp x = read (unwords (tail (take 2 (words x)))) :: TimeStamp
+parseTimestamp x = read (head (words x)) :: TimeStamp
 
 parseMessage :: String -> LogMessage
-parseMessage ('I':' ':xs) = LogMessage (parseMessageType 'I' xs) (parseTimestamp xs) (unwords (drop 2 (words xs)))
-parseMessage ('E':' ':xs) = LogMessage (parseMessageType 'E' xs) (parseTimestamp xs) (unwords (drop 2 (words xs)))
-parseMessage ('W':' ':xs) = LogMessage (parseMessageType 'W' xs) (parseTimestamp xs) (unwords (drop 2 (words xs)))
+parseMessage ('I':' ':xs) = LogMessage (parseMessageType 'I' xs) (parseTimestamp xs) (unwords (drop 1 (words xs)))
+parseMessage ('E':' ':xs) = LogMessage (parseMessageType 'E' xs) (parseTimestamp (unwords (tail (words xs)))) (unwords (drop 2 (words xs)))
+parseMessage ('W':' ':xs) = LogMessage (parseMessageType 'W' xs) (parseTimestamp xs) (unwords (drop 1 (words xs)))
 parseMessage x        = Unknown x
-parseMessage (x:' ':xs)
-
-parseParts :: [String] => LogMessage
-parseParts ('I':' ':y:xs)
 
 parse :: String -> [LogMessage]
 parse x
     | length (lines x) == 1 = [parseMessage x]
     | otherwise = parseMessage (head (lines x)) : parse (unlines (tail (lines x)))
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (LogMessage Unknown x) tree = tree
+insert x (Leaf) = Node Leaf x
+insert (LogMessage Error x y z) (Node ltree LogMessage x y x rtree)
+    | y >= 
